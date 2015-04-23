d454d715de8b43d8be0c0f4504595c97baf33b77
bcarson <bcarson408@gmail.com>
2015-04-23 08:23:49 -0700

    Initial Commit

---
+++ SwiftProject1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftProject1
+//
+//  Created by Brant Carson on 4/23/15.
+//  Copyright (c) 2015 Brant Carson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
