d77076d2016fc5caadd7a41b98cf6d9f7b1d20b7
bcarson <bcarson408@gmail.com>
2015-04-23 09:35:14 -0700

    as so it begins

---
+++ SwiftProject1/BasicStats.swift
@@ -0,0 +1,19 @@
+//
+//  BasicStats.swift
+//  SwiftProject1
+//
+//  Created by Brant Carson on 4/23/15.
+//  Copyright (c) 2015 Brant Carson. All rights reserved.
+//
+
+import Foundation
+
+class Stats {
+    let statYear:Int = 0;
+    let gamesPlayed:Int = 0;
+    let gamesStarted:Int = 0;
+    //    let comp:Int;
+    //    let attempts:Int;
+    //    let yards:Int;
+    //    let td:Int;
+}
---
+++ SwiftProject1/Player.swift
@@ -0,0 +1,23 @@
+//
+//  Player.swift
+//  SwiftProject1
+//
+//  Created by Brant Carson on 4/23/15.
+//  Copyright (c) 2015 Brant Carson. All rights reserved.
+//
+
+import Foundation
+
+
+
+
+class Player {
+    var first_name:String = String()
+    var last_name:String = String()
+    var team = [String]()
+    var position = String()
+    var carearStats = [Stats]();
+    init(first_name:String?,last_name:String?,team:String?,postion:String?,carearStats:Stats?){
+        
+    }
+}
